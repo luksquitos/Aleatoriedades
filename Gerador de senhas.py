3fab09cb87c9854cfa27095176ad50d014ac821c
Lucas <lucasmartinscaet@hotmail.com>
2022-03-15 12:09:59 -0300

    melhorando

--- Gerador de senhas.py
+++ Gerador de senhas.py
@@ -1,18 +1,16 @@
-# Gerador de senhas 
+# Gerador de senhas
 
 from random import choice, randint
 
-random = (0, 1) # Serve apenas para ser chamado dentro do for.
+
 caracteres = 'abcdefghijklmnopqrstuvwxyz1234567890!@#$%&*()'
 password = ''
 
 
 for vezes in range(randint(12, 24)):
-    if choice(random) == 0:
-        try:
-            password += choice(caracteres.upper())
-        except:
-            password += choice(caracteres)
+    random = randint(0, 1) # Serve apenas para gerar um valor aleatório
+    if random:
+        password += choice(caracteres.upper())
     else:
         password += choice(caracteres)
 
